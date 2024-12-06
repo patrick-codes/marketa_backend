ffbbb615546dedd275588db2560bab1375f24dcc
patrick-codes <khofilyndon2018@gmail.com>
2024-12-06 16:47:04 +0000

    category schema fix updated

--- models/categories_model.js
+++ models/categories_model.js
@@ -5,6 +5,10 @@ const categoriesSchema = mongoose.Schema({
     type: String,
     required: [true, ""],
   },
-});
+
+},
+{ timestamps: true }
+);
+
 
 module.exports = mongoose.model("categoriesModel", categoriesSchema);
